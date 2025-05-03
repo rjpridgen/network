bec73424ff2f4783164d51e50f02718c1b93c7da
rjpridgen <encodedrj@icloud.com>
2025-05-03 11:23:41 +0000

    module

--- create_module.ts
+++ create_module.ts
@@ -14,6 +14,6 @@ const github = file(pathToFileURL("./github.json"))
 
 const githubMeta = await github.json() as GithubMeta
 
-const rules = Object.entries(githubMeta).filter(([ key ]) => keys.includes(key)).map(([clave, ips]) => "AND,((OR,(".concat(ips.map((ip) => `IP-${ip.includes("::") ? "CIDR6" : "CIDR"},${ip}`).map((ips) => `(${ips})`).join(', ').concat("))),DIRECT"))).join(", ")
+const rules = Object.entries(githubMeta).filter(([ key ]) => keys.includes(key)).map(([clave, ips]) => "AND,((OR,(".concat(ips.map((ip) => `IP-${ip.includes("::") ? "CIDR6" : "CIDR"},${ip}`).map((ips) => `(${ips})`).join(', ').concat(`))),DIRECT // ${clave}\n`))).join("")
 
 console.log(rules)
